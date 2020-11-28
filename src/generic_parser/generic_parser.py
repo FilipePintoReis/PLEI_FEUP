62dc9ef2b13f408c61879c0b608e236e410b7268
Filipe Pinto Reis <freis@semasio.com>
2020-11-28 21:16:34 +0000

    Added generic parser to work as an abstract

---
+++ src/generic_parser/generic_parser.py
@@ -0,0 +1,20 @@
+'''
+Contains generic parser
+'''
+
+class GenericParser:
+    '''
+    Generic parser
+    '''
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.type = 'Generic'
+
+    def get_annotations(self):
+        '''
+        Get's annotations.
+        Prototype.
+        '''
+        print(f'{self.type} parser.\nMethod not implemented!')
