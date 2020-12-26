e72703dad15c015280d7a813fedc782cb46b4316
Filipe Pinto Reis <freis@semasio.com>
2020-12-26 02:46:17 +0000

    Test files

---
+++ test_embeddings.py
@@ -0,0 +1,53 @@
+'''
+Use data in pre-trained model.
+'''
+
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
+
+import chars2vec
+import sklearn.decomposition
+import matplotlib.pyplot as plt
+
+
+path_to_model = 'trained_models/char_embeddings/tweet'
+
+c2v_model = chars2vec.load_model(path_to_model)
+
+# words = ['Natural', 'Language', 'Understanding',
+#          'Naturael', 'Longuge', 'Updderctundjing',
+#          'Motural', 'Lamnguoge', 'Understaating',
+#          'Naturrow', 'Laguage', 'Unddertandink',
+#          'Nattural', 'Languagge', 'Umderstoneding',
+#          'Whatever', 'Wtv', 'quiero' 'quero',
+#          'amor', 'amorzito', 'amori', 'papa',
+#          'puta', 'puto', 'corazon', 'gracias', 'grazi']
+
+words = ['Natural', 'Language', 'Understanding',
+         'Naturael', 'Longuge', 'Updderctundjing']
+
+# Create word embeddings
+word_embeddings = c2v_model.vectorize_words(words)
+
+print(len(word_embeddings))
+
+# # Project embeddings on plane using the PCA
+# projection_2d = sklearn.decomposition.PCA(n_components=2).fit_transform(word_embeddings)
+
+# # Draw words on plane
+# f = plt.figure(figsize=(8, 6))
+
+# for j in range(len(projection_2d)):
+#     plt.scatter(projection_2d[j, 0], projection_2d[j, 1],
+#                 marker=('$' + words[j] + '$'),
+#                 s=500 * len(words[j]), label=j,
+#                 facecolors='green' if words[j]
+#                             in ['Natural', 'Language', 'Understanding'] else 'black')
+
+# plt.show()
